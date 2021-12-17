17eec8f482b5e6835feda81a4822ca0c60e0f687
jhonboy121 <alfredmathew05@gmail.com>
2021-12-17 17:10:30 +0530

    Settings: allow toggling torch by double/long pressing power button[2/2]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/android/settings/gestures/GestureSettings.java
+++ src/com/android/settings/gestures/GestureSettings.java
@@ -23,7 +23,10 @@ import android.hardware.display.AmbientDisplayConfiguration;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.search.SearchIndexable;
+
+import java.util.List;
 
 @SearchIndexable
 public class GestureSettings extends DashboardFragment {
@@ -52,6 +55,11 @@ public class GestureSettings extends DashboardFragment {
         super.onAttach(context);
         use(AssistGestureSettingsPreferenceController.class).setAssistOnly(false);
         use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return List.of(new DoubleTapPowerTorchPreferenceController(context, getSettingsLifecycle()));
     }
 
     private AmbientDisplayConfiguration getConfig(Context context) {
